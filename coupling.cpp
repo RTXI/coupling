27e733b2c4481c2745204febdfb709cca5e6cc7e
Ansel <aesthapus@gmail.com>
2015-07-21 14:03:19 -0400

    removed qtgui include

--- coupling.cpp
+++ coupling.cpp
@@ -25,7 +25,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <math.h>
 #include <default_gui_model.h>
 #include <main_window.h>
-#include <QtGui>
 
 extern "C" Plugin::Object *createRTXIPlugin(void) {  
 	return new Coupling();
